69b3216eded710f03e777fa36e32b66948960782
Haruki Inoue <haruki@hacarus.com>
2022-12-24 04:36:31 +0900

    update

--- install_tree-sitter.sh
+++ install_tree-sitter.sh
@@ -27,7 +27,8 @@ else
     wget -P tree-sitter ${URL}
     gzip -d "tree-sitter/${FILENAME}"
     mkdir -p ${PATH_TREESITTER}
-    mv "tree-sitter/${FILENAME_WO_GZ}" ${PATH_TREESITTER}
+    mv "tree-sitter/${FILENAME_WO_GZ}" "tree-sitter/tree-sitter"
+    mv "tree-sitter/tree-sitter" ${PATH_TREESITTER}
     rm -rf tree-sitter
 fi
 
