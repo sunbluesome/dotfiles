16a4a1e8b23b7afb0b74448510940bd5cc90b761
Haruki Inoue <haruki@hacarus.com>
2022-12-24 02:13:00 +0900

    update config for treesitter

--- nvim/lua/plugin/syntax/treesitter.lua
+++ nvim/lua/plugin/syntax/treesitter.lua
@@ -4,30 +4,34 @@
 
 local configs = require("nvim-treesitter.configs")
 
-configs.setup({
-    ensure_installed = "all",
-    sync_install = false,
-    ignore_install = { "" }, -- List of parsers to ignore installing
-    highlight = {
-        enable = true, -- false will disable the whole extension
-        disable = { "" }, -- list of language that will be disabled
-        additional_vim_regex_highlighting = true,
-    },
-    autopairs = {
-        enable = true,
-    },
-    indent = { enable = false, disable = { "" } },
+if not vim.fn.has("treesitter") then
+    return
+else
+    configs.setup({
+        ensure_installed = "all",
+        sync_install = false,
+        ignore_install = { "" }, -- List of parsers to ignore installing
+        highlight = {
+            enable = true, -- false will disable the whole extension
+            disable = { "" }, -- list of language that will be disabled
+            additional_vim_regex_highlighting = true,
+        },
+        autopairs = {
+            enable = true,
+        },
+        indent = { enable = false, disable = { "" } },
 
-    rainbow = {
-        enable = true,
-        -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-        extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-        max_file_lines = nil, -- Do not enable for files with more than n lines, int
-        -- colors = {}, -- table of hex strings
-        -- termcolors = {} -- table of colour name strings
-    },
-    context_commentstring = {
-        enable = true,
-        enable_autocmd = false,
-    },
-})
+        rainbow = {
+            enable = true,
+            -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+            extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+            max_file_lines = nil, -- Do not enable for files with more than n lines, int
+            -- colors = {}, -- table of hex strings
+            -- termcolors = {} -- table of colour name strings
+        },
+        context_commentstring = {
+            enable = true,
+            enable_autocmd = false,
+        },
+    })
+end
