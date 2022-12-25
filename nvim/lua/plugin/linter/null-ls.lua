2e4f1669a318aba058d608f631cc8efb0084231b
Haruki Inoue <haruki@hacarus.com>
2022-12-25 21:50:42 +0900

    update

--- nvim/lua/plugin/linter/null-ls.lua
+++ nvim/lua/plugin/linter/null-ls.lua
@@ -4,68 +4,30 @@
 -- LUA:
 --     - stylua for formatting
 -- PYTHON:
+--     - flake8 for diagnostics
+--     - pyptoject_flake8 for diagnostics
+--     - mypy for diagnostics
+--     - pydocstyle for diagnostics
 --     - black for formatting
 --     - isort for formatting
---     - flake8 for diagnostics
---     - mypy for diagnostics
 
 local null_ls = require("null-ls")
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+
 
 local sources = {
     -- lua
     formatting.stylua,
 
     -- python
-    formatting.black.with({
-        extra_args = {
-            "--fast",
-        }
-    }),
-    -- E203: ignored because this causes conflict with `black`. Ref: https://black.readthedocs.io/en/stable/the_black_code_style/current_style.html#slices
-    -- W503: ignored because this rule goes against PEP8. Ref: https://www.flake8rules.com/rules/W503.html
-    diagnostics.flake8.with({
-        extra_args = {
-            "--max-line-length",
-            "88",
-            "--extend-ignore",
-            "D100,D104,D401,E203,W503",
-        },
-    }),
-    diagnostics.pyproject_flake8.with({
-        extra_args = {
-            "--max-line-length",
-            "88",
-            "--extend-ignore",
-            "D100,D104,D401,E203,W503",
-        },
-    }),
-    diagnostics.mypy.with({
-        extra_args = {
-            "--no-implicit-optional",
-            "--ignore-missing-imports",
-            "--check-untyped-defs",
-            "--disallow-untyped-defs",
-            "--warn-unused-ignores",
-            "--exclude",
-            "/(site-packages|node_modules|__pycache__|tests|\\..*)/$",
-        }
-    }),
-    diagnostics.pydocstyle.with({
-        extra_args = {
-            "--convention",
-            "numpy",
-        }
-    }),
-    formatting.isort.with({
-        extra_args = {
-            "--line-length",
-            "88",
-            "--profile",
-            "black",
-        }
-    }),
+    -- automatically use their config file.
+    diagnostics.flake8,
+    -- diagnostics.pyproject_flake8,
+    diagnostics.mypy,
+    diagnostics.pydocstyle,
+    formatting.black,
+    formatting.isort,
 }
 
 
