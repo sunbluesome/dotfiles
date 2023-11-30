ac375d8d4270a7e9855cbc53f78e24c5a748af6e
sunbluesome <h.inoue@full-kaiten.com>
2023-11-30 22:34:18 +0900

    add vscode settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -16,6 +16,38 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- load settings
-require("lazy").setup("plugins")
+
+if vim.g.vscode then
+    require('lazy').setup({
+        spec = {
+            -- common
+            { import = "plugins.comment" },
+            { import = "plugins.hop" },
+            -- For vscode
+        }
+    })
+else
+    require('lazy').setup({
+        spec = {
+            -- common
+            { import = "plugins.comment" },
+            { import = "plugins.gitsigns" },
+            { import = "plugins.hop" },
+            { import = "plugins.bufferline" },
+            -- For neovim
+            { import = "plugins.dap" },
+            { import = "plugins.completion" },
+            { import = "plugins.color-scheme" },
+            { import = "plugins.diffview" },
+            { import = "plugins.lazygit" },
+            { import = "plugins.lualine" },
+            { import = "plugins.null_ls" },
+            { import = "plugins.nvim-ts-rainbow" },
+            { import = "plugins.telescope-file-browser" },
+            { import = "plugins.telescope" },
+            { import = "plugins.treesitter" },
+            { import = "plugins.trouble" },
+        }
+    })
+end
 
