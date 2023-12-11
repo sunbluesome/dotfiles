16a4e4742439379fe43c160f6ee4c86154311b32
sunbluesome <h.inoue@full-kaiten.com>
2023-12-12 01:40:46 +0900

    debug

--- install_lazygit.sh
+++ install_lazygit.sh
@@ -11,7 +11,7 @@ else
     exit 1
 fi
 
-URL="https://github.com/jesseduffield/lazygit/releases/download/v0.42.0/lazygit_0.42.0_${FILENAME}_arm64.tar.gz"
+URL="https://github.com/jesseduffield/lazygit/releases/download/v0.40.2/lazygit_0.40.2_${FILENAME}_arm64.tar.gz"
 FILENAME=${URL##*/}
 FILENAME_WO_GZ=${FILENAME%.*}
 FILENAME_WO_TARGZ=${FILENAME_WO_GZ%.*}
